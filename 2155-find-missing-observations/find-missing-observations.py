849c8621339cd19cf38a392391dc29e742180e2a
Tripti Singh <tripti.singh11111@gmail.com>
2024-09-05 22:45:11 +0530

    Time: 987 ms (60.43%) | Memory: 26.7 MB (69.79%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.py
+++ 2155-find-missing-observations/find-missing-observations.py
@@ -1,7 +1,6 @@
 class Solution:
     def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
         missing_val = (mean * (n + len(rolls))) - sum(rolls)
-        dice = [i for i in range(1, 7)]
         if missing_val > 6*n or missing_val<n:
             return []
         val = missing_val // n
