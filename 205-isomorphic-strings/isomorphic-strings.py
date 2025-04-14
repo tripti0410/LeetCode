718d1ba9c088fd5359409ff883c712889a35cb5a
Tripti Singh <tripti.singh11111@gmail.com>
2025-04-14 22:18:10 +0530

    Time: 3 ms (95.80%) | Memory: 18.1 MB (38.17%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,8 +1,5 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-
         s_2_t = {}
         t_2_s = {}
         for i in range(len(s)):
