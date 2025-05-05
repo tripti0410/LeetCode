08754b965eed09034b7ff147d5ebb1a52beed3eb
Tripti Singh <tripti.singh11111@gmail.com>
2025-05-05 20:02:13 +0530

    Time: 1 ms (77.59%) | Memory: 64.1 MB (74.23%) - LeetSync

---
+++ 189-rotate-array/rotate-array.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    if(nums.length === 1){
+        return nums
+    }
+    if(k > nums.length){
+        k = k % nums.length;
+    }
+    let left = 0
+    let right = nums.length - 1
+    while(left <= right){
+        let temp = nums[left]
+        nums[left] = nums[right]
+        nums[right] = temp
+        left += 1
+        right -= 1
+    }
+    console.log
+    left = 0
+    right = k - 1
+    while(left <= right){
+        let temp = nums[left]
+        nums[left] = nums[right]
+        nums[right] = temp
+        left += 1
+        right -= 1
+    }
+
+    left = k
+    right = nums.length - 1
+    while(left <= right){
+        let temp = nums[left]
+        nums[left] = nums[right]
+        nums[right] = temp
+        left += 1
+        right -= 1
+    }
+    return nums
+};
